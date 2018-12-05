ca25e2d3c470deb5f3be476c3bc9dd0dec8125df
Kim <kimvg2@hotmail.com>
2018-12-05 17:19:25 +0100

    Connect to Mongo
    Error: invalid schema, expected mongodb or mongodb+srv

--- app/routes/killer_routes.js
+++ app/routes/killer_routes.js
@@ -3,7 +3,13 @@ module.exports = function(app, db) {
     // Create route
     app.post('/killers', (req, res) => {
         // create it here
-        console.log(req.body);
-        res.send('Hello');
+        const killer = {photo: req.body.photo, name: req.body.name, alias: req.body.alias, birthdate: req.body.birthdate, birthplace: req.body.birthplace, countryactive: req.body.countryactive, dateactive: req.body.dateactive, victimcount: req.body.victimcount, victims: req.body.victims, motive: req.body.motive, description: req.body.description, status: req.body.status};
+        db.collection('killers').insert(killer, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
 };
--- config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url : mongodb//:kimvg:password123@ds016118.mlab.com:16118/cswp_individueel
-};
